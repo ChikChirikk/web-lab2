6164cb25c99b83546c764b5e88c18a39d06b25db
slamach <drslampro@gmail.com>
2020-10-13 22:39:24 +0300

    Added ControllerServlet

---
+++ src/main/java/slamach/weblab2/ControllerServlet.java
@@ -0,0 +1,26 @@
+package slamach.weblab2;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class ControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+        if (request.getParameter("xval") != null && request.getParameter("yval") != null &&
+                request.getParameter("rval") != null)
+            // TODO: Заменить на AreaCheckServlet
+            getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+        else
+            getServletContext().getRequestDispatcher("/index.html").forward(request, response);
+    }
+}
